b06be27e6682e23990c476fa7c82cf28f348a94b
Naveen Kumar Jha <71990959+jhanaveenk@users.noreply.github.com>
2023-01-23 03:31:00 +0530

    Inheritance and type

---
+++ Type_of_Inheritance.py
@@ -0,0 +1,47 @@
+# Multiple Inheritance
+
+# class Mammal:
+#    def mammal_info(self):
+#       print("Mamal can give direct birth")
+
+
+# class Winged:
+#    def winged_info(self):
+#       print("wingend can fly")
+
+
+# class Bat(Mammal, Winged):
+#    pass
+
+
+# O1 = Bat()
+# O1.mammal_info()
+# O1.winged_info()
+
+
+#Multilevel Inheritance
+
+class SuperClass:
+
+   def super_method(self):
+      print("Super Class method called")
+
+# define class that derive from SuperClass
+class DerivedClass1(SuperClass):
+   def derived1_method(self):
+      print("Derived class 1 method called")
+
+# define class that derive from DerivedClass1
+class DerivedClass2(DerivedClass1):
+
+   def derived2_method(self):
+      print("Derived class 2 method called")
+
+# create an object of DerivedClass2
+d2 = DerivedClass2()
+
+d2.super_method()  # Output: "Super Class method called"
+
+d2.derived1_method()  # Output: "Derived class 1 method called"
+
+d2.derived2_method()  # Output: "Derived class 2 method called"
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+class SuperClass:
+#    def super_method(self):
+#       print('This is Super Class')
+
+
+# class DerivedClass1 (SuperClass):
+#    def derived1_method(self):
+#       print('This is derived Class')
+
+
+# class DerivedClass2 (DerivedClass1):
+#    def derived2_method(self):
+#       print('this is second derived calss')
+
+# o = DerivedClass2
+
+# o.super_method()
+# o.derived1_method()
+# o.derived2_method()
